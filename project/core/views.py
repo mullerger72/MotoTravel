fb0089f822ddee3a85c4b69d76cb739c01c19fd5
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2024-01-08 22:34:17 -0300

    Clase 11 terminada

--- project/core/views.py
+++ project/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
 def index(request):
-    return render(request, "core/index.html", {"nombre": "Ferretería"} )
+    return render(request, "core/index.html")
