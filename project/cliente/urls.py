9708d7bfb3f610a9c22113ccc91b6ce5da966150
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2024-01-15 13:41:34 -0300

    Update urls > appname

--- project/cliente/urls.py
+++ project/cliente/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "cliente"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("pais/list", views.pais_list, name="pais_list"),
--- project/config/urls.py
+++ project/config/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include(("core.urls", "core"))),
-    path("clientes/", include(("cliente.urls", "cliente"))),
+    path("", include("core.urls")),
+    path("clientes/", include("cliente.urls")),
 ]
