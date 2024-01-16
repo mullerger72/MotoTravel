a33cba36b38f15458426e54cb23e3ee5f51fe8db
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2024-01-16 00:14:11 -0300

    update core.views

--- project/core/views.py
+++ project/core/views.py
@@ -4,18 +4,8 @@ from django.urls import reverse
 
 
 def index(request):
-    contexto = {"index": False, "about": False, "clientes": False}
-
-    if request.path == reverse("core:index"):
-        contexto["index"] = True
-    elif request.path == reverse("core:about"):
-        contexto["about"] = True
-    elif request.path == reverse("cliente:index"):
-        contexto["clientes"] = True
-
-    return render(request, "core/index.html", contexto)
+    return render(request, "core/index.html")
 
 
-# crear vista de about.html
 def about(request: HttpRequest) -> HttpResponse:
     return render(request, "core/about.html")
