d968b26ee435a2b5ef32e1040852a3f00ca49713
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2024-01-15 21:22:02 -0300

    Arreglos

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import about, index
 
 app_name = "core"
 
 urlpatterns = [
     path("", index, name="index"),
+    path("about/", about, name="about"),
 ]
--- project/core/views.py
+++ project/core/views.py
@@ -1,5 +1,21 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.urls import reverse
 
 
 def index(request):
-    return render(request, "core/index.html")
+    contexto = {"index": False, "about": False, "clientes": False}
+
+    if request.path == reverse("core:index"):
+        contexto["index"] = True
+    elif request.path == reverse("core:about"):
+        contexto["about"] = True
+    elif request.path == reverse("cliente:index"):
+        contexto["clientes"] = True
+
+    return render(request, "core/index.html", contexto)
+
+
+# crear vista de about.html
+def about(request: HttpRequest) -> HttpResponse:
+    return render(request, "core/about.html")
