0261dafa0dd3f18bddad50d9c8b16dd01c7f4711
mullerger72 <mullerger@gmail.com>
2024-02-07 12:17:51 -0300

    Commit 2024/02/07 - 12:17

--- project/config/urls.py
+++ project/config/urls.py
@@ -2,9 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("root_bitacora/", admin.site.urls),
     path("", include("core.urls")),
     path("bitacora", include("bitacora.urls")),
-#    path("clientes/", include("cliente.urls")),
-#    path("productos/", include("producto.urls")),
 ]
